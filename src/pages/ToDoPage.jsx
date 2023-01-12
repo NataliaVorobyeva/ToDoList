70446698b666f3e70933d7ead213667be5554014
NataliaVorobyeva <nataliavorobyeva5@gmail.com>
2023-01-12 15:29:40 +0100

    update

--- src/pages/ToDoPage.jsx
+++ src/pages/ToDoPage.jsx
@@ -16,5 +16,58 @@ const ToDoPage = () => {
     
   )
 }
+const markDone = (id) => {
+  let newTask = toDo.map( task => {
+      if( task.id === id ) {
+      return ({ ...task, status: !task.status })
+   } 
+      return task
+    })
+     setToDo(newTask)
+     setToDo(toDo.map(task => task.id === id 
+    ? ({ ...task, status: !task.status }) 
+    : (task) 
+  ))
+
+}
+const cancelUpdate = () => {
+  setUpdateData('')
+}
+
+
+const changeHolder = (e) => {
+
+   let newEntry = {
+     id: updateData.id,
+     title: e.target.value,
+     status: updateData.status ? true : false
+   }
+   setUpdateData(newEntry)
+
+  
+  setUpdateData({...updateData, title: e.target.value})
+
+}
+
+const updateTask = () => {
+  
+   let filterRecords = [...toDo].filter( task => task.id !== updateData.id )
+   let updatedObject = [...filterRecords, updateData]
+   setToDo(updatedObject)
+
+  let removeOldRecord = [...toDo].filter(task => task.id !== updateData.id)
+  setToDo([
+    ...removeOldRecord, 
+    updateData
+  ])
+  
+  setUpdateData('')
+
+}
+
+
+
+
+
 
 export default ToDoPage
